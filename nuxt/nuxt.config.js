69dc9e9efa5691b6d3b5f9e9415c948a1a649dff
Koach-jp <kochigtr@gmail.com>
2022-09-26 17:46:20 +0900

    220925

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -56,7 +56,7 @@ export default {
     strategies: {
       'laravelJWT': {
         provider: 'laravel/jwt',
-        url: 'http://localhost:8000',
+        url: process.env.API_URL,
         token: {
           maxAge: 60 * 60
         },
@@ -70,7 +70,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: `${process.env.API_URL}/api/`,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
